f8377256e10b2a57de957c12b1b25dade72d1041
Benjamin Jones <bluefiddleguy@gmail.com>
2019-04-29 20:43:19 -0600

    Images added

--- src/main/angular/talk/src/assets/help_menu.js
+++ src/main/angular/talk/src/assets/help_menu.js
@@ -1,11 +1,14 @@
 function explain_master() {
   var item = document.getElementById("phcb_description");
+  var button = document.getElementById("helpbutton");
   if (item.style.display == "none"
     || item.style.display==undefined) {
     item.style.display = "block";
+    button.className="back";
   }
   else {
     item.style.display = "none";
+    button.className="info";
   }
 }
 

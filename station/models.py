30a10e0fa6ac4c774b43c73bd784e2bbb70bd2d2
Benjamin Jones <bluefiddleguy@gmail.com>
2019-06-02 12:36:52 -0600

    Add callers

--- station/models.py
+++ station/models.py
@@ -17,8 +17,6 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-
-
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
